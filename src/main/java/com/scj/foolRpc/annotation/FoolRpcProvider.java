ecc12d0ab01ad8d1cf7399e36642b9fba73757df
SCJ <suchangjie@SCJdeMacBook-Pro.local>
2023-08-22 21:28:18 +0800

    新增RPC回调模式

---
+++ src/main/java/com/scj/foolRpc/annotation/FoolRpcProvider.java
@@ -0,0 +1,24 @@
+package com.scj.foolRpc.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * @author suchangjie.NANKE
+ * @Title: FoolRpcProvider
+ * @date 2023/8/12 20:05
+ * @description
+ * 标志类为 FoolRpc 的提供方
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface FoolRpcProvider {
+
+    /**
+     * 可扩展字段
+     * @return value
+     */
+    String value() default "";
+}
